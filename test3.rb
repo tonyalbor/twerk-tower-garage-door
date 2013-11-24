ba9d52b101a6d793797d003a706f948af54cbc10
Tony Albor <talbo001@ucr.edu>
2013-11-24 02:09:47 -0800

    use tweet

--- test3.rb
+++ test3.rb
@@ -1,8 +1,19 @@
 require 'rubygems'
 require 'tweetstream'
-require 'json'
+#require 'json'
 require 'oauth'
-
+=begin
+def twerkTweet(twerkStatus)
+    client_twitter = Twitter::Client.new do |config|
+        config.consumer_key        = 'TF2MfJkWtjkJ5a0fBzTF0Q'
+        config.consumer_secret     = 'zh5zvSrd9DHFyd7VQecnTu3iTuUwbheYj9hilsKgUeQ'
+        config.access_token        = '2211707144-coqGW3MUb91RfVXBygz3GEcJD0vhWNZUDeZh4Uy'
+        config.access_token_secret = 'Kw9Psz7EtyyvRS5gFsp7WYxrh787bM3qK4ZPVrHiCNCyI'
+    end
+    
+client_twitter.update("new tweet")
+end
+=end
 TweetStream.configure do |config|
     config.consumer_key       = 'TF2MfJkWtjkJ5a0fBzTF0Q'
     config.consumer_secret    = 'zh5zvSrd9DHFyd7VQecnTu3iTuUwbheYj9hilsKgUeQ'
@@ -19,7 +30,7 @@ tweetOpen = "the garage door is now open"
 tweetClose = "the garage door is now closed"
 
 client = TweetStream::Client.new
-
+idk = 0
 client.userstream do |mention|
     # bool values to verify users and commands
     verifiedUser = 0
@@ -50,7 +61,31 @@ client.userstream do |mention|
     # plus if it actually is a verified command
     if verifiedCommand == 1
         puts "The Twerk Tower will now #{command} the garage door."
+        idk = 1
+        client.update("Dlkdjf;aldjf;lkjf;a")
+        client.stop
+        
         else
         puts "Nah fool"
     end
 end
+
+require 'twitter'
+
+if idk == 1
+    puts "lasdjf;alsdjfl;asdkf"
+    
+    client_twitter = Twitter::Client.new do |config|
+        config.consumer_key        = 'TF2MfJkWtjkJ5a0fBzTF0Q'
+        config.consumer_secret     = 'zh5zvSrd9DHFyd7VQecnTu3iTuUwbheYj9hilsKgUeQ'
+        config.access_token        = '2211707144-coqGW3MUb91RfVXBygz3GEcJD0vhWNZUDeZh4Uy'
+        config.access_token_secret = 'Kw9Psz7EtyyvRS5gFsp7WYxrh787bM3qK4ZPVrHiCNCyI'
+    end
+    
+    client_twitter.update("new tweet")
+
+    
+    
+    #client_twitter.update("new tweeet")
+    #twerkTweet("hello there")
+    end
